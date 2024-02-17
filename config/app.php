eaeeb1b1302f925bab94b7addef57f9e388ad9f2
Victor Hugo <victorhugoraj@gmail.com>
2024-02-17 19:40:00 -0300

    Updated larravel

--- config/app.php
+++ config/app.php
@@ -161,7 +161,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-
+        LaravelFrontendPresets\ArgonPreset\ArgonPresetServiceProvider::class,
         /*
          * Package Service Providers...
          */
